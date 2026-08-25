faef3c66da111fb6be51a21878ccea40fb2dd216
Somakna <somaknasaroeurn@gmail.com>
2026-08-25 14:32:03 +0700

    App.vue

---
+++ SkincareProject/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from '@/views/Header.vue'
+</script>
+
+<template>
+    <h1>Hello</h1>
+ <Header/>
+ <TheWelcome/>
+</template>
+
+
+
